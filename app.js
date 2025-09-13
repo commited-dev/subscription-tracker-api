55aa3db20baa106e433abd109485c2e1e6b14135
commited_dev <devcommited@gmail.com>
2025-09-13 09:48:49 +0200

    create an error middleware, add to server, add the database connection to the server as well

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import { connectToDatabase } from "./database/mongodb.js";
 
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -11,12 +13,16 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
+app.use(errorMiddleware);
+
 app.get("/", (req, res) => {
   res.send("Subscription Tracker API is running");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
