ddd1768ad75952e462071d00f43a4e44e872ab0c
commited_dev <devcommited@gmail.com>
2025-09-13 10:17:34 +0200

    add essential middleware

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
