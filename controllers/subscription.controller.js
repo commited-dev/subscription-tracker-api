9c46d5ef3fdd5bcf5d62f259bb2b01a93b8056e7
commited_dev <devcommited@gmail.com>
2025-09-14 10:33:54 +0200

    create controllers for updating, deleting and canceling a subscriptions

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,5 +1,38 @@
 import Subscription from "../models/subscription.model.js";
 
+export const getAllSubscriptions = async (req, res, next) => {
+  try {
+    const subscriptions = await Subscription.find();
+
+    res.status(200).json({
+      success: true,
+      data: subscriptions,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getSubscriptionById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const subscription = await Subscription.findById(id);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      success: true,
+      data: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const createSubscription = async (req, res, next) => {
   try {
     const subscription = await Subscription.create({
@@ -31,6 +64,79 @@ export const getUserSubscriptions = async (req, res, next) => {
     res.status(200).json({
       success: true,
       data: subscriptions,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Handle updating a subscription by ID here
+export const updateSubscriptionById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const updatedSubscription = await Subscription.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedSubscription) {
+      const error = new Error("Subscription not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      success: true,
+      data: updatedSubscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Handle deleting a subscription by ID here
+export const deleteSubscriptionById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const deletedSubscription = await Subscription.findByIdAndDelete(id);
+
+    if (!deletedSubscription) {
+      const error = new Error("Subscription not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Subscription deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Handle cancel subscription by ID here
+export const cancelSubscriptionById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const subscription = await Subscription.findById(id);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    subscription.status = "canceled";
+    await subscription.save();
+
+    res.status(200).json({
+      success: true,
+      data: subscription,
     });
   } catch (error) {
     next(error);
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,40 +1,30 @@
 import { Router } from "express";
 import { authorize } from "../middlewares/auth.middleware.js";
 import {
+  getAllSubscriptions,
+  getSubscriptionById,
   getUserSubscriptions,
   createSubscription,
+  updateSubscriptionById,
+  deleteSubscriptionById,
+  cancelSubscriptionById,
 } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get("/", (req, res) => {
-  // Handle fetching subscriptions here
-  res.send({ title: "GET all subscriptions" });
-});
+subscriptionRouter.get("/", getAllSubscriptions);
 
-subscriptionRouter.get("/:id", (req, res) => {
-  // Handle fetching a specific subscription by ID here
-  res.send({ title: "GET Subscription by ID" });
-});
+subscriptionRouter.get("/:id", getSubscriptionById);
 
 subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 
-subscriptionRouter.put("/:id", (req, res) => {
-  // Handle updating subscription details here
-  res.send({ title: "UPDATE Subscription by ID" });
-});
+subscriptionRouter.put("/:id", authorize, updateSubscriptionById);
 
-subscriptionRouter.delete("/:id", (req, res) => {
-  // Handle deleting a subscription here
-  res.send({ title: "DELETE Subscription by ID" });
-});
+subscriptionRouter.delete("/:id", authorize, deleteSubscriptionById);
 
-subscriptionRouter.put("/:id/cancel", (req, res) => {
-  // Handle cancel subscription here
-  res.send({ title: "CANCEL Subscription" });
-});
+subscriptionRouter.put("/:id/cancel", authorize, cancelSubscriptionById);
 
 subscriptionRouter.get("/upcoming-renewals", (req, res) => {
   // Handle fetching renewals subscription here
